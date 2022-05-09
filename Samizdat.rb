4f12592d6219547e09ad711511ac907dd23a6ff6
Pedro Arruda <parruda@findhotel.net>
2022-05-09 13:24:54 -0300

    Update Samizdat.rb for distribution

--- Samizdat.rb
+++ Samizdat.rb
@@ -2,8 +2,8 @@ class Samizdat < Formula
     desc "Samizdat: your content, available"
     homepage "https://github.com/tokahuke/samizdat"
     version "0.1.0"
-    url "https://proxy.hubfederation.com/_series/r0Km0HptEt6Fhosmy7qxaKxyDtwHkzi0-eYbt1WatdM/latest/node/aarch64-apple-darwin/samizdat.tar.gz"
-    sha256 "f4e4e1f0a649cc2abf5affd4d42489cb24eb6f385634a55bf28334b715f55500"
+    url "https://proxy.hubfederation.com/_series//latest/node/aarch64-apple-darwin/samizdat.tar.gz"
+    sha256 "c4f63c362cfdff68b3979e8d268e9ba3c9d93187550ba81ce34fd435dc299a1d"
     license "AGPLv3"
 
     def install
