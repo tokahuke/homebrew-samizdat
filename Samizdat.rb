3b982091008c0c637ed17cdaca4ef9fcd7f647e2
samizdat-builder <>
2026-06-08 14:46:51 +0000

    release: bump to 0.3.3 (sha=610ae1bf348af1f59481caa4919174cbd83f2009009e69b92a3eaec814f65a2a)

--- Samizdat.rb
+++ Samizdat.rb
@@ -3,7 +3,7 @@ class Samizdat < Formula
     homepage "https://github.com/tokahuke/samizdat"
     version "0.3.3"
     url "https://series-v5bknud2nujn5bmgrmtmxovrncwhedw4a6jtrnhz4yn3ovm2wxjq.hubfederation.com/0.3.3/aarch64-apple-darwin/samizdat-up/samizdat-up"
-    sha256 "d1da08b9e4f44cc05e3858610885b4fae7fa3b41831569b6de97cfc886484ec9"
+    sha256 "610ae1bf348af1f59481caa4919174cbd83f2009009e69b92a3eaec814f65a2a"
     license "AGPLv3"
     depends_on arch: :arm64
 
